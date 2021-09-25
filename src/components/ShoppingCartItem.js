e7cec6063bef5fbcc593752027c709cf070a989e
markporo <markporo@yahoo.com>
2021-09-25 11:58:46 -0400

    got removeItem working properly by setting filtered array to newCart then passing newCart to setCart

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 
+
 const Item = props => {
 	const [cart, setCart] = useContext(CartContext);
 
-	const removeItem = (e) => {
-		setCart(cart.filter(item => item.id !== e.target.id));
+	const removeItem = (id) => {
+		const newCart = cart.filter(item => item.id !== id);
+		console.log(cart, console.log(newCart));
+
+		setCart(newCart);
 	}
-	console.log(cart);
+
 
 	return (
 		<div className="shopping-cart_item">
@@ -17,7 +21,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button id={props.id} onClick={removeItem}>Remove from cart</button>
+				<button onClick={() => removeItem(props.id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
