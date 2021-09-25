e7c31df54d5f52a2a591480d8136f62f2940099b
markporo <markporo@yahoo.com>
2021-09-25 03:28:30 -0400

    created contexts and used context hook to pass props

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
 
 const Item = props => {
+	const [cart, setCart] = useContext(CartContext);
+
+	const removeItem = (e) => {
+		setCart(cart.filter(item => item.id !== e.target.id));
+	}
+	console.log(cart);
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +17,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button id={props.id} onClick={removeItem}>Remove from cart</button>
 			</div>
 		</div>
 	);
